9fa7f21a3676a2bfbfacae17e5d6a3e8118dade3
90828 <908288050@qq.com>
2025-07-17 19:32:02 +0800

    修bug

--- model/ab_mffe.py
+++ model/ab_mffe.py
@@ -1,47 +1,51 @@
-import cv2
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+class FreBlock(nn.Module):
+    def __init__(self, nc):
+        super(FreBlock, self).__init__()
+        self.processmag = nn.Sequential(
+            nn.Conv2d(nc, nc, 1, 1, 0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(nc, nc, 1, 1, 0))
+        self.processpha = nn.Sequential(
+            nn.Conv2d(nc, nc, 1, 1, 0),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Conv2d(nc, nc, 1, 1, 0))
 
-import cv2 as cv
-from skimage.util import img_as_ubyte
-from torchvision.transforms import transforms
-if __name__ == '__main__':
-    input = cv.imread('D:/Program_Files/stable_diffusion_launcher/outputs/txt2img-images/2023-10-29/00000-1800816867.png')
-    input = cv.cvtColor(input,cv.COLOR_BGR2RGB)
-    x = transforms.ToTensor()(input).unsqueeze(0)
-    # x = nn.functional.batch_norm(x, running_mean=None, running_var=None, training=True, momentum=0.1, eps=1e-5)
-    print(x)
-    fft = torch.fft.fft2(x,norm='ortho')
-    fft = torch.fft.fftshift(fft)
-    # split
-    real, imag = fft.real,fft.imag
-    tester = torch.cat((real,imag),dim=1)
-    net = nn.Sequential(
-        nn.Conv2d(6, 6, kernel_size=3, padding=1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(6, 6, kernel_size=3, padding=1),
-    )
-    output = net(tester)
-    # print(real)
-    # print(imag)
-    # # print max
-    # print(torch.max(real))
-    # print(torch.max(imag))
-    # # print min
-    # print(torch.min(real))
-    # print(torch.min(imag))
-    outr,outi = torch.chunk(output, 2, dim=1)
-    re = torch.fft.ifft2(torch.fft.ifftshift(torch.complex(outr, outi)),norm='ortho')
-    # restored_img = img_as_ubyte(re.real/torch.max(re.real))
-    # cv2.imshow('1',restored_img)
-    print(torch.max(re.imag))
-    print(re.real - x)
+    def forward(self, x):
+        mag = torch.abs(x)
+        pha = torch.angle(x)
+        mag = self.processmag(mag)
+        pha = self.processpha(pha)
+        real = mag * torch.cos(pha)
+        imag = mag * torch.sin(pha)
+        x_out = torch.complex(real, imag)
 
+        return x_out
+
+
+class AB_MFFE(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(AB_MFFE, self).__init__()
+        self.fourier_unit = FreBlock(in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=(1, 1), padding='same')
+        self.relu = nn.LeakyReLU(inplace=True)
+        self.conv_2 = nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=(1, 1), padding='same')
+        self.conv_3 = nn.Conv2d(in_channels*2, out_channels, kernel_size=(1, 1), stride=(1, 1), padding='same')
+
+    def forward(self, x):
+        _, _, H, W = x.shape
+        x_s = self.relu(self.conv_1(x))
+        x_s = x + self.conv_2(x_s)
+
+        x_freq = torch.fft.rfft2(x, norm='backward')
+        x_f = self.fourier_unit(x_freq)
+        x_f = torch.fft.irfft2(x_f, s=(H, W), norm='backward')
+
+        out = self.conv_3(torch.cat((x_s, x_f), dim=1))
+
+        return out
 
 
--- model/backbone_arch.py
+++ model/backbone_arch.py
@@ -11,7 +11,7 @@ class FeatureBlock(nn.Module):
         super(FeatureBlock, self).__init__()
         self.fab1 = FAB(channels)
         self.fab2 = FAB(channels)
-        self.cbam = CBAM(channels)  # Here CBAM is represented with CAB
+        self.cbam = CBAM(channels)
         self.mffe = MFFE(channels)
 
     def forward(self, x):
@@ -49,23 +49,34 @@ class WeightedConnect(nn.Module):
         if not requires_grad:
             self.weights.requires_grad = False
         self.activation = nn.Sigmoid()
-    def forward(self, x):
-        return x * self.activation(self.weights)
+    def forward(self,weighted, x):
+        return weighted * self.activation(self.weights)+x
 
 class UBlock(nn.Module):
     def __init__(self, in_channels=3, base_channels=32,in_height=512,in_width=512,weight_connect=True):
         super(UBlock, self).__init__()
         self.head = nn.Conv2d(in_channels, base_channels, kernel_size=3, padding=1)
 
-        self.encoder1 = nn.Sequential(FeatureBlock(base_channels),DownSample(base_channels, base_channels * 2))
-        self.encoder2 = nn.Sequential(FeatureBlock(base_channels * 2),DownSample(base_channels * 2, base_channels * 4))
-        self.encoder3 = nn.Sequential(FeatureBlock(base_channels * 4),DownSample(base_channels * 4, base_channels * 8))
-        self.encoder4 = nn.Sequential(FeatureBlock(base_channels * 8),DownSample(base_channels * 8, base_channels * 16))
-        self.bottleneck =nn.Sequential(FeatureBlock(base_channels * 16),UpSample(base_channels * 16, base_channels * 8))
-        self.decoder4 = nn.Sequential( FeatureBlock(base_channels * 8),UpSample(base_channels * 8, base_channels * 4))
-        self.decoder3 = nn.Sequential( FeatureBlock(base_channels * 4),UpSample(base_channels * 4, base_channels * 2))
-        self.decoder2 = nn.Sequential( FeatureBlock(base_channels * 2),UpSample(base_channels * 2, base_channels * 1))
-        self.decoder1 = nn.Sequential(FeatureBlock(base_channels))
+        self.eb1 = FeatureBlock(base_channels)
+        self.encoder1 = DownSample(base_channels, base_channels * 2)
+        self.eb2 = FeatureBlock(base_channels * 2)
+        self.encoder2 = DownSample(base_channels * 2, base_channels * 4)
+        self.eb3 = FeatureBlock(base_channels * 4)
+        self.encoder3 = DownSample(base_channels * 4, base_channels * 8)
+        self.eb4 = FeatureBlock(base_channels * 8)
+        self.encoder4 = DownSample(base_channels * 8, base_channels * 16)
+        self.bottleneck = FeatureBlock(base_channels * 16)
+        self.decoder4 = UpSample(base_channels * 16, base_channels * 8)
+        self.db4 = FeatureBlock(base_channels * 8)
+        self.decoder3 = UpSample(base_channels * 8, base_channels * 4)
+        self.db3 = FeatureBlock(base_channels * 4)
+        self.decoder2 = UpSample(base_channels * 4, base_channels * 2)
+        self.db2 = FeatureBlock(base_channels * 2)
+        self.decoder1 = UpSample(base_channels * 2, base_channels)
+        self.db1 = FeatureBlock(base_channels)
+
+        self.tail = nn.Conv2d(base_channels, in_channels, kernel_size=3, padding=1)
+
         c,h,w=in_channels,in_height,in_width
         self.weight0 = WeightedConnect(c,h,w,requires_grad=weight_connect)
         self.weight1 = WeightedConnect(base_channels,h,w,requires_grad=weight_connect)
@@ -73,20 +84,37 @@ class UBlock(nn.Module):
         self.weight3 = WeightedConnect(base_channels*4,h//4,w//4,requires_grad=weight_connect)
         self.weight4 = WeightedConnect(base_channels*8,h//8,w//8,requires_grad=weight_connect)
 
-        self.tail = nn.Conv2d(base_channels, in_channels, kernel_size=3, padding=1)
 
     def forward(self, x):
-        h = self.head(x)
-        e1 = self.encoder1(h)
-        e2 = self.encoder2(e1)
-        e3 = self.encoder3(e2)
-        e4 = self.encoder4(e3)
-        b = self.bottleneck(e4)
-        d4 = self.decoder4(self.weight4(b))
-        d3 = self.decoder3(self.weight3(d4))
-        d2 = self.decoder2(self.weight2(d3))
-        d1 = self.decoder1(self.weight1(d2))
-        t = self.tail(d1)
-        return self.weight0(t)
+        out = self.head(x)
+        v1 = self.eb1(out)
+        out = self.encoder1(v1)
+        v2 = self.eb2(out)
+        out = self.encoder2(v2)
+        v3 = self.eb3(out)
+        out = self.encoder3(v3)
+        v4 = self.eb4(out)
+        out = self.encoder4(v4)
+        out = self.bottleneck(out)
+        out = self.decoder4(out)
+        out = self.weight4(v4,out)
+        out = self.db4(out)
+        out = self.decoder3(out)
+        out = self.weight3(v3,out)
+        out = self.db3(out)
+        out = self.decoder2(out)
+        out = self.weight2(v2,out)
+        out = self.db2(out)
+        out = self.decoder1(out)
+        out = self.weight1(v1,out)
+        out = self.db1(out)
+        out = self.tail(out)
+        return self.weight0(x,out)
 
+
+if __name__ == '__main__':
+    model = UBlock(in_channels=3, base_channels=32,in_height=256,in_width=256,weight_connect=True)
+    x = torch.randn(1, 3, 256, 256)  # Batch size of 1, 3 channels, 512x512 image
+    output = model(x)
+    print(output.shape)  # Should be (1, 3, 512, 512)
 
